5f879a1d3b99dda3f78a0d29eb76248401996d62
Martin <mwendamarto7@gmail.com>
2023-06-27 10:41:12 +0300

    Run a build command

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,4 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>The Home Page </h1>
+
+
+<a href="/about">Go to about</a>
